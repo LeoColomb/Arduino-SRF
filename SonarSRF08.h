c6587881d3fb45102bf50d18a9820940d2def7f3
LeoColomb <leocolombaro@yahoo.fr>
2012-06-21 16:45:55 +0200

    Typo and 'defines'

--- SonarSRF08.h
+++ SonarSRF08.h
@@ -1,7 +1,7 @@
 /*
- SonarSRF08.h - library for controlling SRF08 sonar sensors
- By Zach Foresta April 10, 2009
- By Leo Colomb May 10, 2012
+	SonarSRF08.h - library for controlling SRF08 sonar sensors
+		By Zach Foresta April 10, 2009
+		By Leo Colomb May 10, 2012
 */
 
 #ifndef SonarSRF08_h
@@ -11,17 +11,17 @@
 
 class SonarSRF08
 {
-	public:
-		void connect();
-		void sendCommand(int commandRegister, int address, int command, int gainRegister, int rangeLocation);
-		void setUnit(int commandRegister, int address, char units, int gainRegister, int rangeLocation);
-		void setRegister(int address, int thisRegister);
-		int  readData(int address, int numBytes);
-		void changeAddress(int commandRegister, int NEW_ADDRESS, int gainRegister, int rangeLocation);
+public:
+	void connect();
+	void sendCommand(int commandRegister, int address, int command, int gainRegister, int rangeLocation);
+	void setUnit(int commandRegister, int address, char units, int gainRegister, int rangeLocation);
+	void setRegister(int address, int thisRegister);
+	int  readData(int address, int numBytes);
+	void changeAddress(int commandRegister, int NEW_ADDRESS, int gainRegister, int rangeLocation);
 
-	private:
-		// int _pin1;
-		// int _pin2;
+private:
+	// int _pin1;
+	// int _pin2;
 };
 
 #endif
